a7e4879879f643c010423d9e9c038581644c4f7c
Rajha23 <vijayarajhak@gmail.com>
2026-09-15 22:29:02 +0530

    fix(ui): remove redundant legacy VoiceAssistant component causing icon overlap

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import { ProducePassportModal } from './components/ProducePassportModal';
 import { LanguageSelectorModal } from './components/LanguageSelectorModal';
 import { LanguageOnboardingModal } from './components/LanguageOnboardingModal';
 import { ChatbotWidget } from './components/ChatbotWidget';
-import { VoiceAssistant } from './components/VoiceAssistant';
 
 // Public pages (no sidebar)
 import { LandingPage } from './pages/LandingPage';
@@ -309,7 +308,6 @@ const PublicShell: React.FC = () => (
     <LanguageSelectorModal />
     <LanguageOnboardingModal />
     <ChatbotWidget />
-    <VoiceAssistant />
   </div>
 );
 
@@ -337,7 +335,6 @@ const AuthenticatedShell: React.FC = () => {
       <LanguageSelectorModal />
       <LanguageOnboardingModal />
       <ChatbotWidget />
-      <VoiceAssistant />
     </div>
   );
 };
