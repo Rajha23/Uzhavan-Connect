bfbdd5dec46cc170d88f142eee286f1d97340552
Rajha23 <vijayarajhak@gmail.com>
2026-09-15 21:17:01 +0530

    chore: add global error boundary for production debugging

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import { AppProvider } from './context/AppContext';
 import { LanguageProvider } from './context/LanguageContext';
 import { registerServiceWorker } from './services/serviceWorkerRegistration';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 
 // Register PWA service worker for offline field support
@@ -11,11 +12,13 @@ registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </LanguageProvider>
+    <ErrorBoundary>
+      <LanguageProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </LanguageProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
 
